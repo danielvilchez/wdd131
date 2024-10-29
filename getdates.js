b6533340159635bcb8932120fb55b428ef5e812b
danielvilchez <daniel2005vc@gmail.com>
2024-10-29 12:35:36 -0500

    W01 Assignment: Home Page

---
+++ getdates.js
@@ -0,0 +1,11 @@
+// Obtener el año actual
+const currentYear = new Date().getFullYear();
+// Mostrar el año en el elemento con id "currentyear" con el símbolo de copyright
+document.getElementById("currentyear").textContent = `© ${currentYear}`;
+
+// Obtener la última fecha de modificación del documento
+const lastModifiedDate = document.lastModified;
+// Seleccionar el elemento con el id "lastModified" y mostrar la fecha
+document.getElementById("lastModified").textContent = lastModifiedDate;
+
+
