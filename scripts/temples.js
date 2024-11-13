110a21fae8face80f71a4bd4e3de34aff97a3dc8
danielvilchez <daniel2005vc@gmail.com>
2024-11-13 14:04:53 -0500

    Edit temples script

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,4 @@
+/*
 let d = new Date();
 document.getElementById("currentYear").innerHTML = `&copy;${d.getFullYear()}`;
 document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
@@ -13,3 +14,13 @@ hambutton.addEventListener('click', () => {
 function toggleActive(element) {
     element.classList.toggle("active");
 }
+*/
+// Obtener el año actual
+const currentYear = new Date().getFullYear();
+// Mostrar el año en el elemento con id "currentyear" con el símbolo de copyright
+document.getElementById("currentyear").textContent = `© ${currentYear}`;
+
+// Obtener la última fecha de modificación del documento
+const lastModifiedDate = document.lastModified;
+// Seleccionar el elemento con el id "lastModified" y mostrar la fecha
+document.getElementById("lastModified").textContent = lastModifiedDate;
